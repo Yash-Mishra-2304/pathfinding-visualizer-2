0e7e643c760ad5d812d378c5e853a8e54db1955a
Yash Mishra <96983549+Yash-Mishra-2304@users.noreply.github.com>
2024-09-04 23:36:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import PathfindingVisualizer from './pathfinding-visualizer2.jsx';
 
 function App() {
   return (
